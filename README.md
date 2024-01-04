15fb6c97384f90055d856c50c41275752ce234f8
Paved <pavedngoune@gmail.com>
2024-01-04 19:12:16 +0100

    Affichage des donnees de facon statique

---
+++ README.md
@@ -0,0 +1,6 @@
+### Instal json-server
+* `npm install -g json-server`
+
+### From the command line, at the root of your project run the following commands
+* `npx json-server --watch db.json`
+In your web browser, navigate to the http://localhost:3000/locations and confirm that the response includes the data stored in db.json
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -34,20 +34,25 @@ export class HomeComponent {
   housingLocationList: Housinglocation[] = [];
   filteredLocationList: Housinglocation[] = [];
   housingService: HousingService = inject(HousingService);
-  
+
   filterResults(text: string) {
     if (!text) {
       this.filteredLocationList = this.housingLocationList;
       return;
     }
-  
+
     this.filteredLocationList = this.housingLocationList.filter(
-      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+      (housingLocation) =>
+        housingLocation?.city.toLowerCase().includes(text.toLowerCase())
     );
   }
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
-    this.filteredLocationList = this.housingLocationList;
+    this.housingService
+      .getAllHousingLocations()
+      .then((housingLocationList: Housinglocation[]) => {
+        this.housingLocationList = housingLocationList;
+        this.filteredLocationList = housingLocationList;
+      });
   }
 }
